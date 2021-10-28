f4db707d903e6929abcb3b8b3d0972c745675e77
Алина <alina.12.98@mail.ru>
2021-10-28 17:47:39 +0300

    Half components created

---
+++ src/components/presents/FormForPerson/FormForPerson.jsx
@@ -0,0 +1,12 @@
+import PresentForm from "../PresentForm/PresentForm";
+
+export default function FormForPerson() {
+  return (
+    <>
+    <h2>Ура! Кто-то из твоих близких или знакомых хотят подарить Вам подарок! Заполните, пожалуйста, как можно больше полей</h2>
+    <PresentForm/>
+    <button>Отправить</button>
+    <button>Перейти на сайт</button>
+    </>
+  )
+}
