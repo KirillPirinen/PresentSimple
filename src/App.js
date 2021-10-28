68f251b7416bbb7fc8010771662ef22063dba3c6
AleksanderV <vypovaleksandr@gmail.com>
2021-10-28 17:46:20 +0300

    add mvp lk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import PersonalProfile from "./components/PersonalProfile/PersonalProfile"
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <PersonalProfile />
     </div>
+    </Router>
   );
 }
 
