8e53c86d8fe1d5c6814c16e073fe46fc87c55b11
Kirill <k.pirinen.rus@gmail.com>
2021-10-31 22:18:44 +0300

    to save

--- client/src/components/CheckFormToPerson/CheckFormToPerson.jsx
+++ client/src/components/CheckFormToPerson/CheckFormToPerson.jsx
@@ -1,14 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { checkForm } from "../../redux/actions/checkFormToPerson";
+import { RecipientInfoBlock } from "./subComponents/recipientInfoBlock";
+import { CSSTransition } from 'react-transition-group';
+import { ListOfForms } from "./subComponents/ListOfForms";
 
 export default function CheckFormToPerson() {
   const [inputFormToPerson, setInputFormToPerson] = useState({name: '', lname: '', phone: '', email: ''});
   const {recipient, forms, message} = useSelector(state=>state.checkform)
+  const [localState, setLocalState] = useState({recipient:false, forms:false, message:false})
   const dispatch = useDispatch();
   const history = useHistory();
 
+  useEffect(() => {
+    setLo
+  },[recipient, forms, message])
+  
   const changeHandler = (e) => {
     setInputFormToPerson((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -22,7 +30,8 @@ export default function CheckFormToPerson() {
   };
 
   return(
-    <div>
+    <>
+    <div className='container-glass'>
     <h2>Возможно твой друг уже зарегистрирован здесь и оставил список желаний. Давай проверим</h2>
     <form onSubmit={checkFormHandler} className="d-flex flex-column align-items-center bg-light text-dark p-3 border rounded-3">
       
@@ -74,26 +83,17 @@ export default function CheckFormToPerson() {
       Добавить
     </button>
     </form>
-
+    </div>
+    <div className="container-glass">
     {recipient &&
-    <> 
-      <h2>Отличная новость. Твой друг уже зарегистрирован. Список его желаний доступен на его страничке</h2>
-      <h3>ссылка на пользователя {recipient.name}</h3>
-    </>
+        <RecipientInfoBlock recipient={recipient}/>
     }
+    {forms && <ListOfForms forms={forms}/>}
 
-    {forms && 
-    <>
-      <h2>Хорошая новость! Пользователь уже заполнял анкету.</h2>
-      <ul>
-        {forms.map(e=> <li>f{e.id}</li>)}
-      </ul>
-    </>
-    }
     {message && 
       <h2>{message}</h2>
     }
     </div>
-  
+    </>
   )
 }
