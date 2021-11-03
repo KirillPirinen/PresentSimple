23aabaa0db59718a8cc53cb7491f7af0fe98d6c3
Kirill <k.pirinen.rus@gmail.com>
2021-11-03 17:57:47 +0300

    registration fixed2

--- client/src/components/main/SignUp/SignUp.js
+++ client/src/components/main/SignUp/SignUp.js
@@ -5,6 +5,7 @@ import { signUp } from "../../../redux/actions/user.ac";
 import validator from "validator";
 import { getError, clearError } from "../../../redux/actions/error.ac";
 import ModalInfo from "../../ModalInfo/ModalInfo";
+
 const SignUp = () => {
   const error = useSelector((state) => state.error);
   const [userSignUp, setUserSignUp] = useState({
@@ -42,87 +43,6 @@ const SignUp = () => {
 
   return (
     <div className="d-flex justify-content-center">
-      {error ? (
-        <>
-          <h2>{error}</h2>
-          <form
-            onSubmit={submitHandler}
-            className="d-flex flex-column align-items-center bg-light text-dark p-3 border rounded-3"
-          >
-            <legend className="text-center mb-4">Регистрация</legend>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.name}
-                type="text"
-                name="name"
-                placeholder="Имя"
-              />
-            </div>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.lname}
-                type="text"
-                name="lname"
-                placeholder="Фамилия"
-              />
-            </div>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.email}
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-            </div>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.phone}
-                type="phone"
-                name="phone"
-                placeholder="Телефон"
-              />
-            </div>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.password}
-                type="password"
-                name="password"
-                placeholder="Пароль"
-              />
-            </div>
-
-            <div className="mb-3">
-              <input
-                onChange={changeHandler}
-                className="form-control"
-                value={userSignUp.password2}
-                type="password"
-                name="password2"
-                placeholder="Повторите пароль"
-              />
-            </div>
-
-            <button type="submit" className="btn btn-primary">
-              Зарегистрироваться
-            </button>
-          </form>
-        </>
-      ) : (
         <form
           onSubmit={submitHandler}
           className="d-flex flex-column align-items-center bg-light text-dark p-3 border rounded-3"
@@ -203,7 +123,7 @@ const SignUp = () => {
             history.push('/auth/signin')
           }}>У меня уже есть аккаунт</a>
         </form>
-      )}
+      )
       <ModalInfo/>
     </div>
   );
